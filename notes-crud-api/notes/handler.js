d00c162dc24767370564230aacb0c78bae016361
awpathum <awpathum@gmail.com>
2023-07-10 23:04:41 +0530

    local debug setup done

--- notes-crud-api/notes/handler.js
+++ notes-crud-api/notes/handler.js
@@ -1,10 +1,31 @@
 'use strict';
+const DynamoDB = require("aws-sdk/clients/dynamodb")
+const documentClient = new DynamoDB.DocumentClient({region: 'us-east-1'})
 
-module.exports.createNote = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify("A new note created!")
-  };
+module.exports.createNote = async (event, context, cb) => {
+  let data = JSON.parse(event.body);
+
+  try {
+    const params = {
+      TableName: "notes",
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body,
+      },
+      ConditionExpression: "attribute_not_exists(notesId)",
+    };
+    await documentClient.put(params).promise();
+    cb(null, {
+      statusCode: 201,
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    cb(null, {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    });
+  }
 };
 
 module.exports.updateNote = async (event) => {
