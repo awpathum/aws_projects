3913a464943f8c385fc8099ca0c5ce9f5bf76a16
awpathum <awpathum@gmail.com>
2023-07-15 00:28:12 +0530

    video 53 done

--- notes-crud-api/notes/authorizer.js
+++ notes-crud-api/notes/authorizer.js
@@ -1,41 +1,52 @@
-const generatePolicy = (principalId, effect, resource) =>  {
-    var authResponse = {};
-    authResponse.principalId = principalId;
-    if (effect && resource) {
-        let policyDocument = {
-            Version: "2012-10-17",
-            Statement: [
-                {
-                    Effect:effect,
-                    Resource: resource,
-                    Action: "execute-api:Invoke"
-                }
-            ],
-        };
-        authResponse.policyDocument = policyDocument;
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: "us-east-1_h8dCArtxO",
+  tokenUse: "id",
+  clientId: "1bmp6h7c5d016k5356iev8kaqa",
+});
+
+const generatePolicy = (principalId, effect, resource) => {
+  var authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    let policyDocument = {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: effect,
+          Resource: resource,
+          Action: "execute-api:Invoke",
+        },
+      ],
     };
-    authResponse.context = {
-        foo: "bar"
-    };
-    console.log(JSON.stringify(authResponse));
-    return authResponse;
+    authResponse.policyDocument = policyDocument;
+  }
+  authResponse.context = {
+    foo: "bar",
+  };
+  console.log(JSON.stringify(authResponse));
+  return authResponse;
 };
 
-exports.handler = (event, context, callback) => {
-    // lambda authorizer code
-    var token = event.authorizationToken; // "allow or deny"
-    switch (token) {
-        case "allow":
-            callback(null, generatePolicy("user", "Allow", event.methodArn))
-            break;
-        case "deny":
-            callback(null, generatePolicy("user", "Deny", event.methodArn))
-            break;
-        default:
-            callback("Error: Invalid Token.")
-    }
-}
+exports.handler = async (event, context, callback) => {
+  // lambda authorizer code
+  var token = event.authorizationToken;
+  console.log(token);
+  // validate the token
+  try {
+    const payload = await jwtVerifier.verify(token);
+    console.log(JSON.stringify(payload));
+    callback(null, generatePolicy("user", "Allow", event.methodArn))
+  } catch (error) {
+    callback("Error: Invalid Token");
+  }
+};
 
+if (require.main == module) {
+  console.log("This is a test");
+  exports.handler();
+}
 
 // {
 //     "Version": "2012-10-17",
